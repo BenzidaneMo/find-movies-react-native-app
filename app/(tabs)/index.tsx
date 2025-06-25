5439660d4a24f29c5e69f234a52e5a754386e889
Paragon <hamidouaze@gmail.com>
2025-06-25 12:57:22 +0100

    feat: add initial tab screens for home, profile, saved, and search

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image, Text, View } from "react-native";
+import HeroBanner from "../../components/HeroBanner";
+
+const logo = require('../../assets/images/logo.png');
+
+export default function Index() {
+  return (
+    <View className="relative h-full bg-primary">
+      <HeroBanner />
+      <View className="m-12 items-center"><Image source={logo} className="w-20 h-20"
+                resizeMode="contain"/></View>
+      <Text className="text-light-100 font-bold text-2xl">Edit.</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const search = () => {
+  return (
+    <View className="relative h-full bg-primary">
+      <Text>search</Text>
+    </View>
+  )
+}
+
+export default search
