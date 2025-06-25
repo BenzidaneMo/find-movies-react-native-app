a815636966fb206d4e9afbd624519536f6bff88c
Paragon <hamidouaze@gmail.com>
2025-06-25 12:56:14 +0100

    feat: add icons and images constants, and define TypeScript interfaces for movie data

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import bg from "@/assets/images/bg.webp";
+import highlight from "@/assets/images/highlight.png";
+import rankingGradient from "@/assets/images/rankingGradient.webp";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+};
